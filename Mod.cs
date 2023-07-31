523511fdcce279ef86caac7cc59d3ddda7186596
Markus Chiarot <cmarkus@hotmail.ch>
2023-08-01 00:08:53 +0200

    feat: fix bags, fix translation key, add props some settings

--- Mod.cs
+++ Mod.cs
@@ -9,23 +9,33 @@ namespace SlotMachineNS
     {
         public override void Ready()
         {
-            Logger.Log("Ready!");
             WorldManager.instance.GameDataLoader.AddCardToSetCardBag(SetCardBagType.AdvancedBuildingIdea, "slot_machine_card_idea", 1);
         }
     }
 
     public class SlotMachineCard : CardData
     {
-        public CardBag LootBag;
+        public CardBag LootBag = new CardBag();
 
-        public CardBag ExplodeBag;
+        public CardBag ExplodeBag = new CardBag();
 
         public int ExplodeChance = 0;
 
+        public int ExplodeChangeIncreaseStep = 10;
+
+        public int MaxExplodeChance = 100;
+
+        public int MaxGambleCoinLoot = 3;
+
+        public int MaxCoinExplodeLoot = 5;
+
+        public float GambleTime = 2f;
+
         protected override bool CanHaveCard(CardData otherCard)
         {
-            if (otherCard.Id == "gold" || otherCard.Id == Id)
+            if (otherCard.Id == "gold") // || otherCard.Id == Id TODO: fix multiple slots running at the same time
                 return true;
+
             return base.CanHaveCard(otherCard);
         }
 
@@ -38,7 +48,7 @@ namespace SlotMachineNS
         {
             if (ChildrenMatchingPredicateCount(card => card.Id == "gold") > 0)
             {
-                MyGameCard.StartTimer(2f, Gamble, SokLoc.Translate("slot_machine_card_gamble_status"), "gamble");
+                MyGameCard.StartTimer(GambleTime, Gamble, SokLoc.Translate("slot_machine_card_gamble_status"), "gamble");
             }
             else
             {
@@ -54,17 +64,17 @@ namespace SlotMachineNS
             // destroy 1 gold
             MyGameCard.GetRootCard().CardData.DestroyChildrenMatchingPredicateAndRestack((CardData c) => c.Id == "gold", 1);
 
-            if (new System.Random().Next(1, 100) <= ExplodeChance)
+            if (new System.Random().Next(0, MaxExplodeChance) <= ExplodeChance)
             {
                 MyGameCard.DestroyCard();
             }
             else
             {
                 // increase explode chance
-                ExplodeChance += 10;
+                ExplodeChance += ExplodeChangeIncreaseStep;
 
                 // get random card from LootBag
-                ICardId cardId = LootBag.GetCard(false);
+                ICardId cardId = LootBag.GetCard(removeCard: false);
 
                 // base values
                 int numOfCards = 1;
@@ -73,7 +83,7 @@ namespace SlotMachineNS
                 // modify if card is gold
                 if (cardId.Id == "gold")
                 {
-                    numOfCards = new System.Random().Next(0, 3);
+                    numOfCards = new System.Random().Next(0, MaxGambleCoinLoot + 1);
                     checkAddToStack = true;
                 }
 
@@ -86,7 +96,7 @@ namespace SlotMachineNS
                     WorldManager.instance.CreateSmoke(Position);
 
                     // spawn card(s)
-                    WorldManager.instance.StackSend(card, MyGameCard);
+                    WorldManager.instance.StackSend(card);
                 }
 
             }
@@ -94,19 +104,19 @@ namespace SlotMachineNS
 
         public override void OnDestroyCard()
         {
-            // get random card from LootBag
-            ICardId cardId = ExplodeBag.GetCard(false);
+            // get random card from ExplodeBag
+            ICardId cardId = ExplodeBag.GetCard(removeCard: false);
             int numOfCards = 1;
 
             if (cardId.Id == "gold")
             {
-                numOfCards = new System.Random().Next(0, 5);
+                numOfCards = new System.Random().Next(0, MaxCoinExplodeLoot + 1);
             }
 
             // explode animation
             WorldManager.instance.CreateSmoke(Position);
 
-            // spawn some gold as compensation
+            // spawn something as compensation
             WorldManager.instance.CreateCardStack(Position, numOfCards, cardId.Id, checkAddToStack: false);
 
             base.OnDestroyCard();
